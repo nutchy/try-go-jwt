76f7f633510295a2130e8b50578b000c925f0463
Nutchy <work@nutchy.dev>
2024-07-09 02:10:23 +0700

    feat: add example

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module try-go-jwt
 
 go 1.22.3
+
+require github.com/golang-jwt/jwt/v5 v5.2.1
--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+const SecretKey = "SECRET_KEY"
+
+type CustomClaim struct {
+	Username             string
+	Roles                []string
+	jwt.RegisteredClaims // Registerd claims are predefined fields
+}
 
 func main() {
 	fmt.Println("Hello world")
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, CustomClaim{
+		Username: "john",
+		Roles:    []string{"FullAccess", "ReadWrite"},
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "MyApp",
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(10 * time.Minute)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	})
+
+	t, err := token.SignedString([]byte(SecretKey))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(t)
+
+	// ========
+
+	// Validate token
+	token, err = jwt.Parse(t, func(t *jwt.Token) (interface{}, error) {
+		if _, valid := t.Method.(*jwt.SigningMethodHMAC); !valid {
+			return nil, fmt.Errorf("invalid token %s", token.Header["alg"])
+		}
+		return []byte(SecretKey), nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	if token.Valid {
+		fmt.Println("TOKEN VALID")
+		claims := token.Claims.(jwt.MapClaims)
+		fmt.Println(claims)
+	} else {
+		panic("token invalid")
+	}
 }
